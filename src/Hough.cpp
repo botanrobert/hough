b72069a9bbd75c9d0c605fdac5620534fd9ea6b0
botanrobert <botanrobert@gmail.com>
2018-05-11 19:16:01 +0300

    linies

--- src/Hough.cpp
+++ src/Hough.cpp
@@ -1,11 +1,23 @@
 // Hough.cpp : Defines the entry point for the console application.
 //
 
-#include "stdafx.h"
+//#include "stdafx.h"
 
+#include <opencv2/opencv.hpp>
+#include <opencv2/imgcodecs.hpp>
+
+using namespace cv;
 
 int main()
-{
+{	
+	printf("%s\n", CV_VERSION);
+	Mat imagine;
+	imagine = imread("C:/Users/botan/Documents/GitHub/hough/src/data/linii.png");
+
+	cvNamedWindow("fereastra");
+	imshow("fereastra", imagine);
+	
+	cvWaitKey(0); //pentru a nu inchide fereastra
     return 0;
 }
 
